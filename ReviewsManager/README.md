465757c3e86eaef085f593ad64e3fd14d48f01ba
rinat <rinatpingvin63rus@gmail.com>
2024-02-04 17:27:15 +0400

    Added README.md, Dockerfile, etc

---
+++ ReviewsManager/README.md
@@ -0,0 +1,17 @@
+# ReviewApp
+Django-приложение для отправки/чтения отзывов
+## QuickStart
+```
+git clone https://github.com/RinatNasyrov/ReviewsManager.git
+cd ReviewsManager
+
+python -m venv venv
+venv/Scripts/activate
+python -m pip install -U pip
+
+pip install -r requirements.txt
+```
+Run locally:
+```
+python manage.py runserver
+```
--- ReviewsManager/ReviewsManager/settings.py
+++ ReviewsManager/ReviewsManager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3mmp&p2c_7xrrp*h0hd_@yh+()%en$=&h!8y6#$!i+*93+qm#5'
+env=environ.Env()
+environ.Env().read_env(env_file=os.path.join(BASE_DIR,'.env'))
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
