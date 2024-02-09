43d74ae553140932680e9a72295a1b3bf1b7e1f2
rinat <rinatpingvin63rus@gmail.com>
2024-02-09 17:54:32 +0400

    Using enum for rate field in model, serializer now inherits ModelSerializer

--- ReviewsManager/ReviewsApp/models.py
+++ ReviewsManager/ReviewsApp/models.py
@@ -3,9 +3,17 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.urls import reverse
 
 class Review(models.Model):
+    class Rate(models.IntegerChoices):
+        #OPTIONS = [(1, 'One'), (2, 'Two'), (3, 'Three'), (4, 'Four'), (5, 'Five')]
+        OPTION_1 = 1, '1'
+        OPTION_2 = 2, '2'
+        OPTION_3 = 3, '3'
+        OPTION_4 = 4, '4'
+        OPTION_5 = 5, '5'
+
     name = models.CharField(max_length=32,verbose_name='Имя')
     text = models.TextField(verbose_name='Текст')
-    rate = models.IntegerField(verbose_name='Оценка', validators=[MinValueValidator(1), MaxValueValidator(5)])
+    rate = models.IntegerField(max_length=2, verbose_name='Оценка', choices=Rate.choices)
     def get_absolute_url(self):
         return reverse('list')
 
--- ReviewsManager/ReviewsApp/serializers.py
+++ ReviewsManager/ReviewsApp/serializers.py
@@ -2,11 +2,9 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, Serializer
 from .models import Review
-
-class ReviewSerializer(Serializer):
-    name = serializers.CharField(max_length=32)
-    text = serializers.CharField()
-    rate = serializers.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-
+class ReviewSerializer(ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'name', 'text', 'rate']
     def create(self, validated_data):
         return Review.objects.create(**validated_data)
